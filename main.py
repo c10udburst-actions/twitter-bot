48994e5f3d5cac1877662932941a70be169986d0
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-06-09 13:00:44 +0200

    add comments

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ import re
 import tweepy
 from PIL import Image, ImageDraw, ImageFont
 
+# Objects
 kw_extractor = yake.KeywordExtractor(n=1)
 
 news = fn.CNBC(topics=['finance'])
 
 template = Image.open("./template.jpg")
-
-size = template.width
-font = 400
 
 config = load(open("./config.json"))
 
@@ -25,15 +23,20 @@ auth = tweepy.OAuth1UserHandler(
     access_token=config['access']['key'],
     access_token_secret=config['access']['secret']
 )
-
 twitter = tweepy.API(auth)
 
+# Globals
+size = template.width
+font = 400
+hashtag_re = re.compile("[^A-Za-z0-9]")
+
+
+# Last date
 last_tweet = datetime.fromtimestamp(float(open("./latest.txt", "r").read()))
 curr_date = None
 
-hashtag_re = re.compile("[^A-Za-z0-9]")
-
 for idx, article in enumerate(news.get_news()[::-1]):
+    # skip if already known
     curr_date = datetime(
         year=article.published_parsed.tm_year,
         month=article.published_parsed.tm_mon,
@@ -42,40 +45,43 @@ for idx, article in enumerate(news.get_news()[::-1]):
         minute=article.published_parsed.tm_min,
         second=article.published_parsed.tm_sec
     )
-    
     if curr_date < last_tweet:
         continue
 
+    # create image
     img = template.copy()
     fnt = ImageFont.truetype("./font.ttf", font)
     canvas = ImageDraw.Draw(img)
 
+    # draw text
     lines = "\n".join(textwrap.wrap(article.title, width=font * 256 // size))
-
     canvas.text((size / 2, size / 2), lines, font=fnt, anchor="mm", align="center")
 
+    # figure out hashtags
     keywords = kw_extractor.extract_keywords(f"{article.title} {article.summary}")
     keywords = ["#"+hashtag_re.sub("", key[0]) for key in keywords if key[1] < 0.1]
 
+    # save and upload image
     img.thumbnail((512, 512))
-
     f = io.BytesIO()
     img.save(fp=f, format="png")
     f.seek(0)
-
     media = twitter.media_upload(
         f"article_{idx}.png",
         file=f,
         media_category="TweetImage"
     )
 
+    # figure out tweet content
     content = f"{article.summary}\nSource: {article.link}"
     tags = config['tags'] + keywords
     while len(content) + 1 + len(tags[0]) < 280 and len(tags) > 0:
         content += f" {tags.pop(0)}"
     content = content[:280]
 
+    # tweet
     twitter.update_status(content, media_ids=[media.media_id])
+    print(content)
 
 if curr_date is not None:
     with open("./latest.txt", "w+") as fp:
