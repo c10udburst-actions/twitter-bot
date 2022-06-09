151f881ed17938214597466875c0bb46687c3c40
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-06-09 14:24:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,53 +36,56 @@ last_tweet = datetime.fromtimestamp(float(open("./latest.txt", "r").read()))
 new_last_tweet = 0
 
 for idx, article in enumerate(news.get_news()[::-1]):
-    # skip if already known
-    curr_date = datetime(
-        year=article.published_parsed.tm_year,
-        month=article.published_parsed.tm_mon,
-        day=article.published_parsed.tm_mday,
-        hour=article.published_parsed.tm_hour,
-        minute=article.published_parsed.tm_min,
-        second=article.published_parsed.tm_sec
-    )
-    if curr_date < last_tweet:
-        continue
-    new_last_tweet = max(curr_date.timestamp(), new_last_tweet)
-        
-    # create image
-    img = template.copy()
-    fnt = ImageFont.truetype("./font.ttf", font)
-    canvas = ImageDraw.Draw(img)
+    try:
+        # skip if already known
+        curr_date = datetime(
+            year=article.published_parsed.tm_year,
+            month=article.published_parsed.tm_mon,
+            day=article.published_parsed.tm_mday,
+            hour=article.published_parsed.tm_hour,
+            minute=article.published_parsed.tm_min,
+            second=article.published_parsed.tm_sec
+        )
+        if curr_date < last_tweet:
+            continue
+        new_last_tweet = max(curr_date.timestamp(), new_last_tweet)
 
-    # draw text
-    lines = "\n".join(textwrap.wrap(article.title, width=font * 256 // size))
-    canvas.text((size / 2, size / 2), lines, font=fnt, anchor="mm", align="center")
+        # create image
+        img = template.copy()
+        fnt = ImageFont.truetype("./font.ttf", font)
+        canvas = ImageDraw.Draw(img)
 
-    # figure out hashtags
-    keywords = kw_extractor.extract_keywords(f"{article.title} {article.summary}")
-    keywords = ["#"+hashtag_re.sub("", key[0]) for key in keywords if key[1] < 0.1]
+        # draw text
+        lines = "\n".join(textwrap.wrap(article.title, width=font * 256 // size))
+        canvas.text((size / 2, size / 2), lines, font=fnt, anchor="mm", align="center")
 
-    # save and upload image
-    img.thumbnail((512, 512))
-    f = io.BytesIO()
-    img.save(fp=f, format="png")
-    f.seek(0)
-    media = twitter.media_upload(
-        f"article_{idx}.png",
-        file=f,
-        media_category="TweetImage"
-    )
+        # figure out hashtags
+        keywords = kw_extractor.extract_keywords(f"{article.title} {article.summary}")
+        keywords = ["#"+hashtag_re.sub("", key[0]) for key in keywords if key[1] < 0.1]
 
-    # figure out tweet content
-    content = f"{article.summary}\nSource: {article.link}"
-    tags = config['tags'] + keywords
-    while len(content) + 1 + len(tags[0]) < 280 and len(tags) > 0:
-        content += f" {tags.pop(0)}"
-    content = content[:280]
+        # save and upload image
+        img.thumbnail((512, 512))
+        f = io.BytesIO()
+        img.save(fp=f, format="png")
+        f.seek(0)
+        media = twitter.media_upload(
+            f"article_{idx}.png",
+            file=f,
+            media_category="TweetImage"
+        )
 
-    # tweet
-    twitter.update_status(content, media_ids=[media.media_id])
-    print(content)
+        # figure out tweet content
+        content = f"{article.summary}\nSource: {article.link}"
+        tags = config['tags'] + keywords
+        while len(tags) > 0 and len(content) + 1 + len(tags[0]) < 280:
+            content += f" {tags.pop(0)}"
+        content = content[:280]
+
+        # tweet
+        twitter.update_status(content, media_ids=[media.media_id])
+        print(content)
+    except Exception as ex:
+        print(f"\t{ex}")
 
 if new_last_tweet > 0:
     with open("./latest.txt", "w+") as fp:
